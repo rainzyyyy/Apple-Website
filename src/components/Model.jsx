701c220ba5a56f731ee204a9f30b0de44ccddc31
rainzyyyy <hew78pikrou@gmail.com>
2024-05-19 17:17:39 +0800

    Fixed typo

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -54,7 +54,7 @@ const Model = () => {
 
     return (
         <section className='common-padding'>
-            <div class='screen-max-width'>
+            <div className='screen-max-width'>
                 <h1 id='heading' className='section-heading'>Take a closer look.</h1>
 
                 <div className='flex flex-col items-center mt-5'>
