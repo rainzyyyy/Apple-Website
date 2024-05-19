f9c8e3260c9c121e1eb7714bf9bb21da5f5091cf
rainzyyyy <hew78pikrou@gmail.com>
2024-05-19 17:25:14 +0800

    Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import Navbar from "./components/Navbar";
+import Highlights from "./components/Highlights";
+import Hero from "./components/Hero";
+import Model from "./components/Model";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Footer from "./components/Footer";
+
+import * as Sentry from "@sentry/react";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
